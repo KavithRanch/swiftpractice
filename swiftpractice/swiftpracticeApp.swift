bcd94f34632cdadf38cf61f857322b5206bcc009
KavithRanch <kavithranch@gmail.com>
2026-03-22 10:33:40 -0400

    Initial Commit

---
+++ swiftpractice/swiftpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftpracticeApp.swift
+//  swiftpractice
+//
+//  Created by Kavith Ranchagoda on 2026-03-22.
+//
+
+import SwiftUI
+
+@main
+struct swiftpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
