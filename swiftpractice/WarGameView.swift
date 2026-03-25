13153e039532e6c01a89f5415dbaf658396f436b
KavithRanch <kavithranch@gmail.com>
2026-03-25 19:40:34 -0400

    Began working on War Card Game

---
+++ swiftpractice/WarGameView.swift
@@ -0,0 +1,18 @@
+//
+//  WarGameView.swift
+//  swiftpractice
+//
+//  Created by Kavith Ranchagoda on 2026-03-25.
+//
+
+import SwiftUI
+
+struct WarGameView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    WarGameView()
+}
