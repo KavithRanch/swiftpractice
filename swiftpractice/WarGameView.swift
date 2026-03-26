563abd244bb1325c30124a2c432749534c040481
KavithRanch <kavithranch@gmail.com>
2026-03-25 20:03:55 -0400

    Initial Game Layout

--- swiftpractice/WarGameView.swift
+++ swiftpractice/WarGameView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct WarGameView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Image("background-cloth")
+            
+            VStack(spacing:45){
+                Image("logo")
+                HStack(spacing: 40){
+                    Image("card2")
+                    Image("card9")
+                }
+                Image("button")
+                
+                HStack(spacing:90){
+                    VStack(spacing:25){
+                        Text("Player").font(Font.title2)
+                        Text("0").font(Font.largeTitle.bold())
+                    }
+                    VStack(spacing:25){
+                        Text("CPU").font(Font.title2)
+                        Text("0").font(Font.largeTitle.bold())
+                    }
+                }
+                .foregroundStyle(Color(.white))
+            }
+        }
     }
 }
 
